6cae3e8412be21b528d3bc64ef4184798ef25716
Guilherme de Mazzi <guilherme.mazzi@enforcegroup.com.br>
2024-05-21 10:53:40 -0300

    feat: created mutation code and refactor

--- crossover.py
+++ crossover.py
@@ -1,12 +1,7 @@
 import random
 import sys
 
-OCORRE_MUTACAO = 0.1
-SELECIONAR_MELHOR_CROSSOVER = 0.63
-SELECIONAR_PIOR_CROSSOVER = 0.27
-TIPO_GENE_SELECAO = 0.77
-INDIVIDUO_MUTACAO = 0.57
-TAXA_MUTACAO = 0.8
+from utils import CrossoverHelper
 
 
 class Item:
@@ -35,8 +30,9 @@ class Individuo:
 
     def get_weight(self) -> int:
         weight = 0
-        for item in self.itens:
-            weight += item.peso
+        weight += ITEM_1.peso * self.item1
+        weight += ITEM_2.peso * self.item2
+        weight += ITEM_3.peso * self.item3
 
         return weight
 
@@ -49,14 +45,15 @@ class Individuo:
         return sum_value
 
     def __str__(self) -> str:
-        return f"Nome: {self.nome} | Item 1: {self.item1} | Item 2: {self.item2} | Item 3: {self.item3}"
+        return f"Nome: {self.nome} | Item 1: {self.item1} | Item 2: {self.item2} | Item 3: {self.item3} | Valor: {self.get_value()} | Peso: {self.get_weight()}"
 
 
+# Itens definidos
 ITEM_1 = Item(nome="1", peso=3, valor=100, qnt=7)
 ITEM_2 = Item(nome="2", peso=6, valor=200, qnt=2)
 ITEM_3 = Item(nome="3", peso=4, valor=50, qnt=5)
 
-
+# População definida
 populacao_list = [
     Individuo(nome="1", item1=2, item2=2, item3=4),
     Individuo(nome="2", item1=4, item2=1, item3=3),
@@ -64,116 +61,132 @@ populacao_list = [
     Individuo(nome="4", item1=1, item2=2, item3=4),
 ]
 
-# print(populacao_list[3].get_weight())
-# print(populacao_list[3].get_value())
+# Porcentagens originalmente definidas aleatoriamente
+OCORRE_MUTACAO = 0.1
+SELECIONAR_MELHOR_CROSSOVER = 0.63
+SELECIONAR_PIOR_CROSSOVER = 0.27
+TIPO_GENE_SELECAO = 0.77
+INDIVIDUO_MUTACAO = 0.57
+TAXA_MUTACAO = 0.08
+GENE_A_SER_MUTADO = 0.45
+QNTD_GENE_MUTACAO = 0.58
 
 
-def sort_by_valor_sum(individuo):
-    sum_value = 0
-    sum_value += ITEM_1.valor * individuo.item1
-    sum_value += ITEM_2.valor * individuo.item2
-    sum_value += ITEM_3.valor * individuo.item3
-    return sum_value
+def init_app():
+    helper = CrossoverHelper(ITEM_1, ITEM_2, ITEM_3)
+    print("1º Etapa - Criação Indivíduos")
+    print()
+    print("INDIVÍDUOS")
+    for index, individuo in enumerate(populacao_list):
+        # print("---------------------------------------------")
+        print(individuo)
+    print("_____________________________________________")
 
+    """ CLASSIFICAÇÃO
+    """
+    # Ordenando população baseado no valor somado (quanto maior, mais a cima)
+    sorted_populacao = sorted(
+        populacao_list, key=helper.sort_by_valor_sum, reverse=True
+    )
 
-def sort_by_weight_sum(individuo):
-    return sum(item.peso for item in individuo.itens)
+    print("2º Etapa - Classificação")
+    print()
+    print("INDIVÍDUOS ORDENADOS POR VALOR")
+    for index, individuo in enumerate(sorted_populacao):
+        # print("---------------------------------------------")
+        print(individuo)
+    print("_____________________________________________")
 
+    # Dividindo população pelos melhores e piores (divido por 2)
+    midpoint = len(sorted_populacao) // 2
+    best_half = sorted_populacao[:midpoint]
+    worst_half = sorted_populacao[midpoint:]
 
-""" CLASSIFICAÇÃO
-"""
-sorted_populacao = sorted(populacao_list, key=sort_by_valor_sum, reverse=True)
+    # Definindo o melhor e o pior, baseado na porcentagem definida
+    best_selected = helper.select_to_crossover(
+        best_half, SELECIONAR_MELHOR_CROSSOVER, worst_half=False
+    )
+    worst_selected = helper.select_to_crossover(
+        worst_half, SELECIONAR_PIOR_CROSSOVER, worst_half=True
+    )
 
-midpoint = len(sorted_populacao) // 2
-best_half = sorted_populacao[:midpoint]
-worst_half = sorted_populacao[midpoint:]
+    print("3º Etapa - Selecionar indivíduo para crossover")
+    print()
+    print(f"MELHOR SELECIONADO (porcentagem = {SELECIONAR_MELHOR_CROSSOVER})")
+    print(best_selected)
 
+    print(f"PIOR SELECIONADO (porcentagem = {SELECIONAR_PIOR_CROSSOVER})")
+    print(worst_selected)
+    print("_____________________________________________")
 
-def select_to_crossover(half: list, percentage, worst_half=False):
-    sum_value = half[0].get_value() + half[1].get_value()
-    likely_first = half[0].get_value() / sum_value
-    likely_last = half[1].get_value() / sum_value
+    """ CROSSOVER
+    """
+    print("4º Etapa - crossover")
+    print()
+    selected_gene = helper.select_gene(TIPO_GENE_SELECAO)
+    print(
+        f"ITEM {selected_gene} A SOFRER CROSSOVER (porcentagem = {TIPO_GENE_SELECAO})"
+    )
+    # Invertendo genes baseado na porcentagem definida.
+    best_selected, worst_selected = helper.swap_genes(
+        selected_gene, best_selected, worst_selected
+    )
 
-    difference_first = abs(likely_first - percentage)
-    difference_last = abs(likely_last - percentage)
+    print(f"MELHOR SELECIONADO APÓS CROSSOVER")
+    print(best_selected)
+    print(f"PIOR SELECIONADO APÓS CROSSOVER")
+    print(worst_selected)
+    print("_____________________________________________")
 
-    if worst_half:
-        if likely_first == 0.5:
-            selected_value = half[0]
-        elif difference_first < difference_last:
-            selected_value = half[1]
+    """ MUTAÇÃO
+    """
+    print("5º Etapa - Mutação")
+    print()
+    if OCORRE_MUTACAO < TAXA_MUTACAO:
+        print("NAO OCORRE MUTACAO")
+        sys.exit()
+
+    # Qual sofre mutacao
+    individuo_a_ser_mutado = helper.select_mutation_individuo(
+        [best_selected, worst_selected], INDIVIDUO_MUTACAO
+    )
+    print(f"INDIVIDUO A SER MUTADO (porcentagem = {INDIVIDUO_MUTACAO})")
+    print(individuo_a_ser_mutado)
+
+    print()
+    # Qual gene a ser mutado
+    index = helper.select_gene(GENE_A_SER_MUTADO)
+    print(f"GENE A SER MUTADO (porcentagem = {INDIVIDUO_MUTACAO})")
+    print(f"Item {index}")
+
+    print()
+    # Quantidade de genes a ser mutado
+    gene_mutado = helper.qntd_to_change_gene(index, QNTD_GENE_MUTACAO)
+    print(f"QUANTIDADE DE GENE A SER MUTADO (porcentagem = {QNTD_GENE_MUTACAO})")
+    print(gene_mutado)
+
+    print()
+    # Modificando o individuo com o gene modificado
+    individuo_a_ser_mutado = helper.change_gene(
+        index, individuo_a_ser_mutado, gene_mutado
+    )
+    print(f"INDIVIDUO APÓS MUTAÇÃO")
+    print(individuo_a_ser_mutado)
+    print("_____________________________________________")
+
+    """ RESULTADO FINAL, COM A MUTAÇÃO FEITA
+    """
+    print()
+    print("INDIVÍDUOS APÓS CROSSOVER")
+    for index, individuo in enumerate(populacao_list):
+        # print("---------------------------------------------")
+        if individuo.nome == individuo_a_ser_mutado.nome:
+            populacao_list[index] = individuo_a_ser_mutado
+            print(individuo_a_ser_mutado)
         else:
-            selected_value = half[0]
-    else:
-        if likely_first == 0.5:
-            selected_value = half[0]  # ERRADO, era pra ser 1 ao inves de 0
-        elif difference_first < difference_last:
-            selected_value = half[0]
-        else:
-            selected_value = half[1]
-
-    return selected_value
+            print(individuo)
+    print("_____________________________________________")
 
 
-best_selected = select_to_crossover(
-    best_half, SELECIONAR_MELHOR_CROSSOVER, worst_half=False
-)
-worst_selected = select_to_crossover(
-    worst_half, SELECIONAR_PIOR_CROSSOVER, worst_half=True
-)
-
-""" CROSSOVER
-"""
-
-
-def select_gene_crossover(percentage):
-    if percentage < 1 / 3:
-        return 1
-    elif percentage < 2 / 3:
-        return 2
-    else:
-        return 3
-
-
-def swap_genes(position, best_selected, worst_selected):
-    if position == 1:
-        best_item = best_selected.item1
-        worst_item = worst_selected.item1
-        best_selected.item1 = worst_item
-        worst_selected.item1 = best_item
-    elif position == 2:
-        best_item = best_selected.item2
-        worst_item = worst_selected.item2
-        best_selected.item2 = worst_item
-        worst_selected.item2 = best_item
-    else:
-        best_item = best_selected.item3
-        worst_item = worst_selected.item3
-        best_selected.item3 = worst_item
-        worst_selected.item3 = best_item
-
-    return best_selected, worst_selected
-
-
-crossover_result = swap_genes(
-    select_gene_crossover(TIPO_GENE_SELECAO), best_selected, worst_selected
-)
-
-for i in crossover_result:
-    print(i)
-
-""" Mutação
-"""
-if OCORRE_MUTACAO < TAXA_MUTACAO:
-    print("NAO OCORRE MUTACAO")
-    sys.exit()
-
-# qual sofre mutacao
-
-
-# quak gene a ser mutado
-
-# quantidade a ser mutada
-
-""" RESULTADO FINAL, COM A MUTAÇÃO FEITA
-"""
+if __name__ == "__main__":
+    init_app()
---
+++ utils.py
@@ -0,0 +1,97 @@
+class CrossoverHelper:
+    def __init__(self, ITEM_1, ITEM_2, ITEM_3):
+        self.ITEM_1 = ITEM_1
+        self.ITEM_2 = ITEM_2
+        self.ITEM_3 = ITEM_3
+
+    def sort_by_valor_sum(self, individuo):
+        sum_value = 0
+        sum_value += self.ITEM_1.valor * individuo.item1
+        sum_value += self.ITEM_2.valor * individuo.item2
+        sum_value += self.ITEM_3.valor * individuo.item3
+        return sum_value
+
+    def sort_by_weight_sum(self, individuo):
+        return sum(item.peso for item in individuo.itens)
+
+    def select_to_crossover(self, half: list, percentage, worst_half=False):
+        sum_value = half[0].get_value() + half[1].get_value()
+        likely_first = half[0].get_value() / sum_value
+        likely_last = half[1].get_value() / sum_value
+
+        difference_first = abs(likely_first - percentage)
+        difference_last = abs(likely_last - percentage)
+
+        if worst_half:
+            if likely_first == 0.5:
+                selected_value = half[0]
+            elif difference_first < difference_last:
+                selected_value = half[1]
+            else:
+                selected_value = half[0]
+        else:
+            if likely_first == 0.5:
+                selected_value = half[0]  # ERRADO, era pra ser 1 ao inves de 0
+            elif difference_first < difference_last:
+                selected_value = half[0]
+            else:
+                selected_value = half[1]
+
+        return selected_value
+
+    def select_gene(self, percentage):
+        if percentage < 1 / 3:
+            return 1
+        elif percentage < 2 / 3:
+            return 2
+        else:
+            return 3
+
+    def swap_genes(self, position, best_selected, worst_selected):
+        if position == 1:
+            best_item = best_selected.item1
+            worst_item = worst_selected.item1
+            best_selected.item1 = worst_item
+            worst_selected.item1 = best_item
+        elif position == 2:
+            best_item = best_selected.item2
+            worst_item = worst_selected.item2
+            best_selected.item2 = worst_item
+            worst_selected.item2 = best_item
+        else:
+            best_item = best_selected.item3
+            worst_item = worst_selected.item3
+            best_selected.item3 = worst_item
+            worst_selected.item3 = best_item
+
+        return best_selected, worst_selected
+
+    def select_mutation_individuo(self, populacao_list, percentage):
+        index = 1 if percentage > 0.50 else 0
+        return populacao_list[index]
+
+    # quantidade a ser mutada
+    def qntd_to_change_gene(self, index, percentage):
+        if index == 1:
+            max_qnt = self.ITEM_1.qnt
+        elif index == 2:
+            max_qnt = self.ITEM_2.qnt
+        elif index == 3:
+            max_qnt = self.ITEM_3.qnt
+
+        step_size = 1 / (max_qnt + 1)
+
+        # Determine which range the percentage falls into
+        for i in range(max_qnt + 1):
+            if percentage <= (i + 1) * step_size:
+                return i
+
+    def change_gene(self, index, individuo, gene):
+        if index == 1:
+            individuo.item1 = gene
+        elif index == 2:
+            individuo.item2 = gene
+        elif index == 3:
+            individuo.item3 = gene
+
+        return individuo
